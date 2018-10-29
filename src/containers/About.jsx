0b469c3ae26fdcf1a4722e3d47ba586ed00defb1
Pooja Soni <pooja.soni@neosofttech.com>
2018-10-29 19:47:17 +0530

    aa

---
+++ src/containers/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => (
+  <div>
+    <h2>About Page</h2>
+    <p>This app is about React/redux</p>
+  </div>
+);
+
+export default About;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/*
+  issue with redux-logger and react-hot-loader
+  even tho those 2 deps are only used in development
+  eslint has no way to tell that and outputs an error
+*/
+
+// react deps
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// hot reload for development
+import { AppContainer } from 'react-hot-loader';
+
+// redux deps
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+import reducer from './reducers';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './style.scss';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const middleware = [thunk];
+if (process.env.NODE_ENV !== 'production') middleware.push(logger);
+
+const store = createStore(
+  reducer,
+  applyMiddleware(...middleware),
+);
+
+const root = document.getElementById('root');
+
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Provider store={store}>
+        <Component />
+      </Provider>
+    </AppContainer>,
+    root,
+  );
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./App', () => { render(App); });
+  module.hot.accept('./reducers', () => { store.replaceReducer(reducer); });
+}
